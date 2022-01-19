a2685c930eef1abd515cabc9638c0b06b513a406
James.Keogh <jkeogh@adga.ca>
2022-01-19 13:48:16 -0500

    adds controllers

--- src/main/java/com/example/demo1/Scene2Controller.java
+++ src/main/java/com/example/demo1/Scene2Controller.java
@@ -1,8 +1,21 @@
 package com.example.demo1;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Scene2Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent parent;
 
+public void switchToScene1(ActionEvent event)
+{
+
+}
+    public void switchToScene2(ActionEvent event)
+    {
+
+    }
 }
