3d97dada403c7e45bdb638b4c5956bbd0b10d5d8
James.Keogh <jkeogh@adga.ca>
2022-01-19 13:36:21 -0500

    initial commit with renamed files

---
+++ src/main/java/com/example/demo1/SceneSwitchMain.java
@@ -0,0 +1,23 @@
+package com.example.demo1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneSwitchMain extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader root = new FXMLLoader(SceneSwitchMain.class.getResource("scene.fxml"));
+        Scene scene = new Scene(root.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
