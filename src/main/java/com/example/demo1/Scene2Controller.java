3910e3abf9df76ee4fff71bf54001d83ca2267ad
DublinJim <dublinjim.jk@gmail.com>
2023-01-17 18:03:37 -0500

    added a function

--- src/main/java/com/example/demo1/Scene2Controller.java
+++ src/main/java/com/example/demo1/Scene2Controller.java
@@ -1,35 +1,34 @@
 package com.example.demo1;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Scene2Controller {
-    private Stage stage;
-    private Scene scene;
-    private Parent parent;
 
-    @FXML
-    private Label lbl1;
     @FXML
     private Button btn1;
 
+
+
+
     @FXML
     public void switchToScene2() throws IOException {
-        FXMLLoader root = new FXMLLoader(SceneSwitchMain.class.getResource("scene.fxml"));
+
+        getTheScene("scene.fxml");
+    }
+
+    private void getTheScene(String fxmlFile) throws IOException {
+        FXMLLoader root = new FXMLLoader(SceneSwitchMain.class.getResource(fxmlFile));
         Stage stage = (Stage) btn1.getScene().getWindow();
         stage.setScene(new Scene(root.load()));
     }
 
 }
-
 
 
 //stage=((Node)event.getSource()).getScene().getWindow();
